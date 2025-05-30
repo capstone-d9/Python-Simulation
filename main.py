1a07214d197d0a11d161621cf3efb7c5562a81c3
alexchtst-01 <acinatra@gmail.com>
2025-05-30 14:55:00 +0700

    feat: refactor water quality simulation initialization with sensor configuration

--- main.py
+++ main.py
@@ -1,8 +1,18 @@
 from utils import WaterWualitySimulation
-import matplotlib.pyplot as plt
 
-waterSim = WaterWualitySimulation()
-waterSim.initSensor()
-waterSim.simulate(num_sim=12)
-# print(waterSim.tell())
-waterSim.AnimateFrame()
+ecosystem = WaterWualitySimulation(
+    sensor_args = [
+        {'name': 'region1', 'xpos': 138, 'ypos':413, 'color': [255, 0, 0], 'size':(15, 15)},
+        {'name': 'region2', 'xpos': 138, 'ypos':138, 'color': [0, 255, 0], 'size':(15, 15)},
+        {'name': 'region3', 'xpos': 413, 'ypos':138, 'color': [0, 0, 255], 'size':(15, 15)},
+        {'name': 'region4', 'xpos': 413, 'ypos':413, 'color': [255, 0, 255], 'size':(15, 15)},
+    ],
+)
+
+# must do
+ecosystem.initSensor()
+ecosystem.simulate(num_sim=24*2)
+
+# ecosystem.AnimateSensors(sensor_names=['region1', 'region3'])
+# ecosystem.AnimateDOOverview()
+
