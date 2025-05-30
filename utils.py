c498e8a76ab7bcc2509f6ce01e114aa2cdcc5147
alexchtst-01 <acinatra@gmail.com>
2025-05-30 14:41:07 +0700

    feat: update water quality simulation with sensor system and DO estimation

--- utils.py
+++ utils.py
@@ -148,10 +148,10 @@ class WaterWualitySimulation:
     def __init__(
         self,
         sensor_args = [
-            {'name': 'pH1', 'xpos': 138, 'ypos':413, 'color': [255, 0, 0], 'size':(15, 15)},
-            {'name': 'pH2', 'xpos': 138, 'ypos':138, 'color': [255, 0, 0], 'size':(15, 15)},
-            {'name': 'pH3', 'xpos': 413, 'ypos':138, 'color': [255, 0, 0], 'size':(15, 15)},
-            {'name': 'pH4', 'xpos': 413, 'ypos':413, 'color': [255, 0, 0], 'size':(15, 15)},
+            {'name': 'system1', 'xpos': 138, 'ypos':413, 'color': [255, 0, 0], 'size':(15, 15)},
+            {'name': 'system2', 'xpos': 138, 'ypos':138, 'color': [0, 255, 0], 'size':(15, 15)},
+            {'name': 'system3', 'xpos': 413, 'ypos':138, 'color': [0, 0, 255], 'size':(15, 15)},
+            {'name': 'system4', 'xpos': 413, 'ypos':413, 'color': [255, 0, 255], 'size':(15, 15)},
         ],
         pond_args = {'width': 550, 'height': 550, 'color':  [177, 220, 234]},
     ):
@@ -164,6 +164,9 @@ class WaterWualitySimulation:
         
         self.sensors = sensor_args
         self.__calculated_do_rate_change__ = {}
+        self.__calcutaed_estimated_do__ = {}
+        self.__temp_sensor__ = {}
+        self.__pH_sensor__ = {}
     
     def initSensor(self):
         for sensor in self.sensors:
@@ -175,121 +178,205 @@ class WaterWualitySimulation:
             self.initial__frame[ypos_temp - height_temp // 2:ypos_temp + height_temp // 2, xpos_temp - width_temp // 2:xpos_temp + width_temp // 2] = sensor['color']
             self.__frameDatas__.append(self.initial__frame)
             self.__calculated_do_rate_change__[sensor['name']] = []
-            
-    def calcutaleDoRateChange(
-        self, 
-        pH,
-        T,
-        NH4 = 1.0,
-        DO = 5.0,
-        params= {
-            'k_a20': 0.5, 'theta_a': 1.024,
-            'r_nitr': 4.57, 'theta_n': 1.08, 'pK_a': 6.5, 'pK_b': 9.0,
-            'mu_max': 10.0, 'theta_f': 1.07, 'I': 1000.0, 'K_I': 200.0, 'A': 5.0,
-            'a': 24.715, 'B': 0.5, 'W': 100.0
-        }
-    ):
-        # Reaerasi
-        k_a = params['k_a20'] * params['theta_a']**(T - 20)
+            self.__calcutaed_estimated_do__[sensor['name']] = []
+            self.__temp_sensor__[sensor['name']] = []
+            self.__pH_sensor__[sensor['name']] = []
+    
+    def calculateDOEstimation(self, rate_change, do_before, interval):
+        return rate_change * interval + do_before
+    
+    def dDOdt_reduced(self, DO, T, pH, params=None, debug=False):
+        """
+        Estimasi laju perubahan DO (mg/L/hari) menggunakan model tereduksi
+        hanya dengan input suhu (T) dan pH berdasarkan sintesis dari Mwegoha et al. (2010).
+        
+        Args:
+            DO (float): Kondisi DO awal (mg/L)
+            T (float): Suhu air (°C)
+            pH (float): pH air
+            params (dict, optional): Parameter model yang dapat dikalibrasi.
+                Keys:
+                - k_r20: reaerasi pada 20°C (1/hari)
+                - theta_r: faktor suhu reaerasi
+                - mu_max: laju maksimum fotosintesis (mg/L/hari)
+                - B_alg: biomassa alga (mg/L)
+                - K_pH: konstanta pH untuk fotosintesis
+                - pH_opt: pH optimal
+                - R0: respirasi ikan dasar (mg/L/hari)
+                - k_T: eksponen suhu respirasi ikan
+                - R_max: respirasi fitoplankton per biomassa (1/hari)
+                - k20: biodegradasi COD pada 20°C (1/hari)
+                - theta_d: faktor suhu biodegradasi
+                - K_DO: half-saturation DO untuk biodegradasi
+                - COD0: konsentrasi COD (mg/L)
+        Returns:
+            dDO/dt (mg/L/hari)
+        """
+        # Default parameters (kalibrasi sesuaikan dengan data lapangan)
+        if params is None:
+            params = {
+                'k_aeration': 0.2,
+                'k_r20': 0.0015, # sesuai
+                'theta_r': 1.024, # sesuai
+                'mu_max': 2.2, # sesuai
+                'B_alg': 10.0,
+                'K_pH': 200, # sesuai
+                'pH_opt': 6.8, # sesuai
+                'R0': 3.0,
+                'k_T': 0.063,
+                'R_max': 1.0, 
+                # 'R_max': 0.5, # sesuai
+                'k20': 0.0015, # sesuai
+                'theta_d': 0.25,
+                'K_DO': 0.10, # sesuai
+                'COD0': 50.0
+            }
+        
+        # (I) Reaerasi
+        k_r = (params['k_r20'] + params['k_aeration']) * params['theta_r']**(T - 20)
         DO_sat = 14.652 - 0.41022*T + 0.007991*T**2 - 0.000077774*T**3
-        reaerasi = k_a * (DO_sat - DO)
+        reaerasi = k_r * (DO_sat - DO)
         
-        # Nitrifikasi
-        fT_n = params['theta_n']**(T - 20)
-        fpH = 1 / (1 + 10**(params['pK_a'] - pH)) * 1 / (1 + 10**(pH - params['pK_b']))
-        nitrifikasi = params['r_nitr'] * fT_n * fpH * NH4
+        # (II) Fotosintesis (diimplementasikan dengan pH dan suhu)
+        f_T = params['theta_r']**(T - 20)
+        f_pH = params['K_pH'] / (params['K_pH'] + abs(pH - params['pH_opt']))
+        fotosintesis = params['mu_max'] * params['B_alg'] * f_T * f_pH
         
-        # Fotosintesis (siang, I/(I+K_I) ≈ 1 jika I >> K_I)
-        fT_f = params['theta_f']**(T - 20)
-        fotosintesis = params['mu_max'] * fT_f * (params['I']/(params['I'] + params['K_I'])) * params['A']
+        # (III) Respirasi ikan
+        respirasi_ikan = params['R0'] * np.exp(params['k_T'] * T)
         
-        # Respirasi ikan
-        respirasi = params['a'] * params['B'] * params['W']**(-0.237) * np.exp(0.063 * T)
+        # (IV) Respirasi fitoplankton
+        # respirasi_fito = params['R_max'] * params['B_alg'] * f_T
+        respirasi_fito = params['R_max'] * params['B_alg'] * f_T
+        
+        # (V) Biodegradasi bahan organik
+        biodegradasi = params['k20'] * params['theta_d']**(T - 20) * (DO / (DO + params['K_DO'])) * params['COD0']
+        
+        # Total
+        dDOdt = reaerasi + fotosintesis - respirasi_ikan - respirasi_fito
+        
+        if debug:
+            print("===============================")
+            print(f"dDO/dt: {dDOdt}")
+            print(f"reaerasi: {reaerasi / 24}")
+            print(f"fotosintesis: {fotosintesis / 24}")
+            print(f"respirasi_ikan: {respirasi_ikan / 24}")
+            print(f"respirasi_fito: {respirasi_fito / 24}")
+            print(f"biodegradasi: {biodegradasi / 24}")
+        
+        return dDOdt
     
-        return reaerasi - nitrifikasi + fotosintesis - respirasi
-    
-    def RandomInputSensor(self, time, pH_initial = 6, T_initial = 26):
-        pH = random.random() * (8 - pH_initial) + pH_initial
-        T = self.PickTemp(time, base_T=T_initial)
+    def tempSimulation(self, t):
+        suhu_min = 24.5
+        suhu_max = 34.0
+        T = suhu_min + (suhu_max - suhu_min) * np.sin((np.pi / 24) * (t - 6))
+        T = np.clip(T, suhu_min, suhu_max)
         
-        calculated_do_rate_change = self.calcutaleDoRateChange(pH=pH, T=T) / 24
-        
-        return calculated_do_rate_change
+        return T
     
-    # time in minutes
-    def PickTemp(self, time, base_T = 6):
-        # Siang hari (pukul 06:00 - 18:00)
-        if 6 * 60 <= time <= 18 * 60:
-            # Normalisasi waktu siang dari 0 ke pi (pagi ke sore)
-            normalized_time = (time - 6 * 60) / (12 * 60) * m.pi
-            # Suhu naik mengikuti kurva sinus dari base_T (min) ke base_T + delta_T (maks)
-            delta_T = 4  # kenaikan suhu maksimal dari suhu dasar saat siang
-            return base_T + delta_T * m.sin(normalized_time)
+    def pHSimulation(self, t=None):
+        pH_rata2 = 6.5
+        fluktuasi = 0.7
+        pH = pH_rata2 + fluktuasi * random.random()
+        pH = np.clip(pH, 6.5, 8.5)
         
-        else:
-            return base_T
+        return pH
     
-    def simulate(self, num_sim=24*60):
-        for time in tqdm(range(num_sim)):
-            if time % 60 == 0:
-                pass
-            
+    def simulate(self, num_sim=24):
+        initialDO = {sensor['name']: random.uniform(3, 5) for sensor in self.sensors}
+
+        for time in range(num_sim):
             self.__frameDatas__.append(self.initial__frame)
             for key in self.__calculated_do_rate_change__:
-                temp = self.RandomInputSensor(time, pH_initial=random.randint(6, 8), T_initial=random.randint(26, 30))
-                self.__calculated_do_rate_change__[key].append(temp)
+                
+                pH = self.pHSimulation(t=time) + random.random() * 1.2 - 0.6
+                T = self.tempSimulation(t=time) + random.random() * 2 - 1
+                ddo_dt = self.dDOdt_reduced(DO=initialDO[key], pH=pH, T=T) / 24
+                DO_estimated = initialDO[key] + ddo_dt
+                
+                # Simpan data
+                self.__calculated_do_rate_change__[key].append(ddo_dt)
+                self.__pH_sensor__[key].append(pH)
+                self.__temp_sensor__[key].append(T)
+                self.__calcutaed_estimated_do__[key].append(DO_estimated)
+
+                initialDO[key] = DO_estimated
 
     def AnimateFrame(self, saveMode=False, file_path=None):
         if file_path is None:
             file_path = f"result/result{datetime.now().strftime('%Y%m%d%H%M%S')}.gif"
 
-        fig = plt.figure(figsize=(12, 6))
-        gs = gridspec.GridSpec(2, 3, figure=fig)
+        fig, ax = plt.subplots(2, 3, figsize=(12, 6))
+        ax = ax.flatten()
 
-        ax0 = fig.add_subplot(gs[:, 0])
-        ax0.set_title("Simulation")
-        ax0.axis('off')
+        # Titles
+        ax[0].set_title("Simulation Temp Condition")
+        ax[1].set_title("Temperature Monitoring")
+        ax[2].set_title("pH Monitoring")
+        ax[3].set_title("Simulation pH Condition")
+        ax[4].set_title("DO Rate Change")
+        ax[5].set_title("Estimated DO Monitoring")
 
-        # Grafik DO Rate Changes
-        ax1 = fig.add_subplot(gs[0, 1])
-        ax2 = fig.add_subplot(gs[0, 2])
-        ax3 = fig.add_subplot(gs[1, 1])
-        ax4 = fig.add_subplot(gs[1, 2])
+        # Turn off axis for image plots
+        ax[0].axis('off')
+        ax[3].axis('off')
 
-        # Set title dan axis DO
-        axs = [ax1, ax2, ax3, ax4]
-        max_val = max([max(value) for key, value in self.__calculated_do_rate_change__.items()])
-        min_val = min([min(value) for key, value in self.__calculated_do_rate_change__.items()])
-        
-        len_data = max([len(value) for key, value in self.__calculated_do_rate_change__.items()])
+        # Set plot limits
+        num_frames = len(self.__frameDatas__)
+        ax[1].set_xlim(0, num_frames)
+        ax[2].set_xlim(0, num_frames)
+        ax[4].set_xlim(0, num_frames)
+        ax[5].set_xlim(0, num_frames)
 
-        for ax in axs:
-            ax.set_xlabel("Time Hour")
-            ax.set_ylabel("Rate Changes (mg/L·jam)")
-            ax.set_ylim(min_val - 0.5,
-                        max_val + 0.5)
-            ax.set_xlim(0, len_data)
+        ax[1].set_ylim(15, 35)
+        ax[2].set_ylim(6, 8)
+        ax[4].set_ylim(-0.5, 0.5)
+        ax[5].set_ylim(-1, 5)
 
-        t = np.linspace(1, len_data + 2, num=len_data)
+        # Image plots
+        im0 = ax[0].imshow(self.__frameDatas__[0])
+        im1 = ax[3].imshow(self.__frameDatas__[0])
 
-        im = ax0.imshow(self.initial__frame)
-        line1, = ax1.plot([], [], color='blue')
-        line2, = ax2.plot([], [], color='blue')
-        line3, = ax3.plot([], [], color='blue')
-        line4, = ax4.plot([], [], color='blue')
-        
+        # Prepare line plots
+        lines = {sensor['name']: {} for sensor in self.sensors}
+        for sensor in self.sensors:
+            color = np.array(sensor['color']) / 255.0
+            name = sensor['name']
+
+            lines[name]['T'], = ax[1].plot([], [], label=name, color=color)
+            lines[name]['pH'], = ax[2].plot([], [], label=name, color=color)
+            lines[name]['DO_rate'], = ax[4].plot([], [], label=name, color=color)
+            lines[name]['DO_est'], = ax[5].plot([], [], label=name, color=color)
+
+        for i in [1, 2, 4, 5]:
+            ax[i].legend(loc='upper right', fontsize='small')
+
         def update(idx):
-            im.set_array(self.__frameDatas__[idx])
-            for key, line in zip(self.__calculated_do_rate_change__, [line1, line2, line3, line4]):
-                line.set_data(t[:idx], self.__calculated_do_rate_change__[key][:idx])
-            return [im, line1, line2, line3, line4]
+            im0.set_array(self.__frameDatas__[idx])
+            im1.set_array(self.__frameDatas__[idx])
+
+            for sensor in self.sensors:
+                name = sensor['name']
+                x = list(range(idx + 1))
+
+                if len(self.__pH_sensor__[name]) > idx:
+                    lines[name]['pH'].set_data(x, self.__pH_sensor__[name][:idx + 1])
+                if len(self.__temp_sensor__[name]) > idx:
+                    lines[name]['T'].set_data(x, self.__temp_sensor__[name][:idx + 1])
+                if len(self.__calculated_do_rate_change__[name]) > idx:
+                    lines[name]['DO_rate'].set_data(x, self.__calculated_do_rate_change__[name][:idx + 1])
+                if len(self.__calcutaed_estimated_do__[name]) > idx:
+                    lines[name]['DO_est'].set_data(x, self.__calcutaed_estimated_do__[name][:idx + 1])
+
+            return [im0, im1] + [line for line_dict in lines.values() for line in line_dict.values()]
 
         plt.tight_layout()
-        ani = FuncAnimation(fig, update, frames=len(self.__frameDatas__), interval=200)
+        ani = FuncAnimation(fig, update, frames=num_frames, interval=200, blit=False)
+
         if saveMode:
             dir_path = os.path.dirname(file_path)
             if dir_path:
                 os.makedirs(dir_path, exist_ok=True)
             ani.save(file_path)
-        
+
         plt.show()
