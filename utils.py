1bc93c1c60f06d54f70794fdad8899ffd4bc3365
alexchtst-01 <acinatra@gmail.com>
2025-05-30 14:55:05 +0700

    feat: update water quality simulation sensor configuration and enhance animation methods

--- utils.py
+++ utils.py
@@ -148,10 +148,10 @@ class WaterWualitySimulation:
     def __init__(
         self,
         sensor_args = [
-            {'name': 'system1', 'xpos': 138, 'ypos':413, 'color': [255, 0, 0], 'size':(15, 15)},
-            {'name': 'system2', 'xpos': 138, 'ypos':138, 'color': [0, 255, 0], 'size':(15, 15)},
-            {'name': 'system3', 'xpos': 413, 'ypos':138, 'color': [0, 0, 255], 'size':(15, 15)},
-            {'name': 'system4', 'xpos': 413, 'ypos':413, 'color': [255, 0, 255], 'size':(15, 15)},
+            {'name': 'region1', 'xpos': 138, 'ypos':413, 'color': [255, 0, 0], 'size':(15, 15)},
+            {'name': 'region2', 'xpos': 138, 'ypos':138, 'color': [0, 255, 0], 'size':(15, 15)},
+            {'name': 'region3', 'xpos': 413, 'ypos':138, 'color': [0, 0, 255], 'size':(15, 15)},
+            {'name': 'region4', 'xpos': 413, 'ypos':413, 'color': [255, 0, 255], 'size':(15, 15)},
         ],
         pond_args = {'width': 550, 'height': 550, 'color':  [177, 220, 234]},
     ):
@@ -302,7 +302,7 @@ class WaterWualitySimulation:
 
                 initialDO[key] = DO_estimated
 
-    def AnimateFrame(self, saveMode=False, file_path=None):
+    def AnimateAllFrame(self, saveMode=False, file_path=None):
         if file_path is None:
             file_path = f"result/result{datetime.now().strftime('%Y%m%d%H%M%S')}.gif"
 
@@ -369,6 +369,150 @@ class WaterWualitySimulation:
                     lines[name]['DO_est'].set_data(x, self.__calcutaed_estimated_do__[name][:idx + 1])
 
             return [im0, im1] + [line for line_dict in lines.values() for line in line_dict.values()]
+
+        plt.tight_layout()
+        ani = FuncAnimation(fig, update, frames=num_frames, interval=200, blit=False)
+
+        if saveMode:
+            dir_path = os.path.dirname(file_path)
+            if dir_path:
+                os.makedirs(dir_path, exist_ok=True)
+            ani.save(file_path)
+
+        plt.show()
+
+    def AnimateSensors(self, sensor_names, saveMode=False, file_path=None):
+        for sensor_name in sensor_names:
+            if sensor_name not in [s['name'] for s in self.sensors]:
+                raise ValueError(f"Sensor '{sensor_name}' not found in self.sensors.")
+
+        if file_path is None:
+            timestamp = datetime.now().strftime('%Y%m%d%H%M%S')
+            joined_names = "_".join(sensor_names)
+            file_path = f"result/result_{joined_names}_{timestamp}.gif"
+
+        fig, ax = plt.subplots(2, 3, figsize=(12, 6))
+        ax = ax.flatten()
+
+        ax[0].set_title("Simulation Temp Condition")
+        ax[1].set_title("Temperature Monitoring")
+        ax[2].set_title("pH Monitoring")
+        ax[3].set_title("Simulation pH Condition")
+        ax[4].set_title("DO Rate Change")
+        ax[5].set_title("Estimated DO Monitoring")
+
+        ax[0].axis('off')
+        ax[3].axis('off')
+
+        num_frames = len(self.__frameDatas__)
+        for i in [1, 2, 4, 5]:
+            ax[i].set_xlim(0, num_frames)
+
+        ax[1].set_ylim(15, 35)
+        ax[2].set_ylim(6, 8)
+        ax[4].set_ylim(-0.5, 0.5)
+        ax[5].set_ylim(-1, 5)
+
+        # Image plots
+        im0 = ax[0].imshow(self.__frameDatas__[0])
+        im1 = ax[3].imshow(self.__frameDatas__[0])
+
+        lines = {name: {} for name in sensor_names}
+        for sensor_name in sensor_names:
+            sensor = next(s for s in self.sensors if s['name'] == sensor_name)
+            color = np.array(sensor['color']) / 255.0
+
+            lines[sensor_name]['T'], = ax[1].plot([], [], label=sensor_name, color=color)
+            lines[sensor_name]['pH'], = ax[2].plot([], [], label=sensor_name, color=color)
+            lines[sensor_name]['DO_rate'], = ax[4].plot([], [], label=sensor_name, color=color)
+            lines[sensor_name]['DO_est'], = ax[5].plot([], [], label=sensor_name, color=color)
+
+        for i in [1, 2, 4, 5]:
+            ax[i].legend(loc='upper right', fontsize='small')
+
+        def update(idx):
+            im0.set_array(self.__frameDatas__[idx])
+            im1.set_array(self.__frameDatas__[idx])
+            x = list(range(idx + 1))
+
+            for name in sensor_names:
+                if len(self.__pH_sensor__[name]) > idx:
+                    lines[name]['pH'].set_data(x, self.__pH_sensor__[name][:idx + 1])
+                if len(self.__temp_sensor__[name]) > idx:
+                    lines[name]['T'].set_data(x, self.__temp_sensor__[name][:idx + 1])
+                if len(self.__calculated_do_rate_change__[name]) > idx:
+                    lines[name]['DO_rate'].set_data(x, self.__calculated_do_rate_change__[name][:idx + 1])
+                if len(self.__calcutaed_estimated_do__[name]) > idx:
+                    lines[name]['DO_est'].set_data(x, self.__calcutaed_estimated_do__[name][:idx + 1])
+
+            return [im0, im1] + [line for sensor_line in lines.values() for line in sensor_line.values()]
+
+        plt.tight_layout()
+        ani = FuncAnimation(fig, update, frames=num_frames, interval=200, blit=False)
+
+        if saveMode:
+            dir_path = os.path.dirname(file_path)
+            if dir_path:
+                os.makedirs(dir_path, exist_ok=True)
+            ani.save(file_path)
+
+        plt.show()
+
+    def AnimateDOOverview(self, saveMode=False, file_path=None):
+        if file_path is None:
+            file_path = f"result/do_overview_{datetime.now().strftime('%Y%m%d%H%M%S')}.gif"
+
+        fig, ax = plt.subplots(2, 2, figsize=(10, 6))
+        ax = ax.flatten()
+
+        # Set judul untuk tiap subplot
+        ax[0].set_title("Simulation Temp Frame")
+        ax[1].set_title("DO Rate Change")
+        ax[2].set_title("Simulation pH Frame")
+        ax[3].set_title("Estimated DO")
+
+        # Image axes (frame suhu dan pH)
+        ax[0].axis('off')
+        ax[2].axis('off')
+
+        # Line axes
+        num_frames = len(self.__frameDatas__)
+        ax[1].set_xlim(0, num_frames)
+        ax[1].set_ylim(-0.5, 0.5)
+
+        ax[3].set_xlim(0, num_frames)
+        ax[3].set_ylim(-1, 5)
+
+        # Image data (gunakan frame yang sama untuk suhu dan pH untuk sekarang)
+        im_temp = ax[0].imshow(self.__frameDatas__[0])
+        im_pH = ax[2].imshow(self.__frameDatas__[0])
+
+        # Siapkan line untuk DO rate dan DO estimated
+        lines = {sensor['name']: {} for sensor in self.sensors}
+        for sensor in self.sensors:
+            name = sensor['name']
+            color = np.array(sensor['color']) / 255.0
+
+            lines[name]['DO_rate'], = ax[1].plot([], [], label=name, color=color)
+            lines[name]['DO_est'], = ax[3].plot([], [], label=name, color=color)
+
+        ax[1].legend(loc='upper right', fontsize='small')
+        ax[3].legend(loc='upper right', fontsize='small')
+
+        def update(idx):
+            im_temp.set_array(self.__frameDatas__[idx])
+            im_pH.set_array(self.__frameDatas__[idx])
+
+            for sensor in self.sensors:
+                name = sensor['name']
+                x = list(range(idx + 1))
+
+                if len(self.__calculated_do_rate_change__[name]) > idx:
+                    lines[name]['DO_rate'].set_data(x, self.__calculated_do_rate_change__[name][:idx + 1])
+                if len(self.__calcutaed_estimated_do__[name]) > idx:
+                    lines[name]['DO_est'].set_data(x, self.__calcutaed_estimated_do__[name][:idx + 1])
+
+            return [im_temp, im_pH] + [line for d in lines.values() for line in d.values()]
 
         plt.tight_layout()
         ani = FuncAnimation(fig, update, frames=num_frames, interval=200, blit=False)
